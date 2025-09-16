ab13cb92b98435e8e8ba4dfc3119209027919f74
LORENZOCIRCELLI <lorenzocircelli5@gmail.com>
2025-09-16 18:39:40 -0300

    Transaction updated

--- src/main/java/com/foundationbank/project/model/Transaction.java
+++ src/main/java/com/foundationbank/project/model/Transaction.java
@@ -1,12 +1,26 @@
 package com.foundationbank.project.model;
 
 import jakarta.persistence.GenerationType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.foundationbank.project.model.base.Auditable;
+import com.foundationbank.project.model.enums.TransactionType;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -14,10 +28,37 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Table(name = "transactions")
-public class Transaction {
+@EqualsAndHashCode(callSuper = false)
+public class Transaction extends Auditable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @NotNull(message = "An amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
+    private BigDecimal amount;
+
+    private String description;
+
+    private LocalDate transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+    
+    @ManyToOne
+    @JoinColumn(name = "related_account_id")
+    private Account relatedAccount;
+
+    public Transaction(TransactionType transactionType, BigDecimal amount, String description, LocalDate transactionDate){
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.description = description;
+        this.transactionDate = transactionDate;
+    }
 
 }
