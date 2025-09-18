b445a04eb9798d86dfb5108550cd598f3192b19f
LORENZOCIRCELLI <lorenzocircelli5@gmail.com>
2025-09-18 14:50:44 -0300

    Repository Annotation Added to Model Repositories

--- src/main/java/com/foundationbank/project/Repositories/TransactionRepository.java
+++ src/main/java/com/foundationbank/project/Repositories/TransactionRepository.java
@@ -1,9 +1,13 @@
 package com.foundationbank.project.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.foundationbank.project.model.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    
+    List<Transaction> findByAccountAccountId(Long accountId);
 }
--- src/main/java/com/foundationbank/project/Repositories/UserRepository.java
+++ src/main/java/com/foundationbank/project/Repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.foundationbank.project.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.foundationbank.project.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
     
 }
