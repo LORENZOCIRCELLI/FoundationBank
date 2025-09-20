57910bf3e23e10eb3c8d0dd4d8c01d2f1f291c2f
Lorenzo Circelli <113491223+LORENZOCIRCELLI@users.noreply.github.com>
2025-09-20 12:09:14 -0300

    Account, User and Transaction Controllers Created

--- src/main/java/com/foundationbank/project/Controller/AccountController.java
+++ src/main/java/com/foundationbank/project/Controller/AccountController.java
@@ -1,8 +1,63 @@
 package com.foundationbank.project.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/account")
+import com.foundationbank.project.Services.AccountService;
+import com.foundationbank.project.payload.DTO.AccountDTO;
+import com.foundationbank.project.payload.Response.AccountResponse;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+
+
+@RestController()
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    AccountService accountService;
     
+    @PostMapping("/create/{userId}")
+    public ResponseEntity<AccountDTO> createAccount(@Valid @RequestBody AccountDTO accountDTO, @PathVariable Long userId){
+
+        AccountDTO savedAccountDTO = accountService.createAccount(accountDTO, userId);
+        return new ResponseEntity<>(savedAccountDTO, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<AccountResponse> getAllAccounts(){
+        AccountResponse accountResponse = accountService.getAllAccounts();
+        return new ResponseEntity<>(accountResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{accountId}")
+    public ResponseEntity<AccountResponse> getAccountById(@PathVariable Long accountId) {
+        AccountResponse accountResponse = accountService.getAccountById(accountId);
+        return new ResponseEntity<AccountResponse>(accountResponse, HttpStatus.OK);
+    }
+    
+    @PutMapping("/update/{accountId}")
+    public ResponseEntity<AccountDTO> updateAccount(@Valid @RequestBody AccountDTO accountDTO, @PathVariable Long accountId){
+        AccountDTO savedAccountDTO = accountService.updateAccount(accountDTO, accountId);
+        return new ResponseEntity<>(savedAccountDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/remove/{accountId}")
+    public ResponseEntity<AccountDTO> deleteAccount(@PathVariable Long accountId){
+        AccountDTO deletedAccount = accountService.deleteAccount(accountId);
+        return new ResponseEntity<>(deletedAccount, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/foundationbank/project/Controller/UserController.java
+++ src/main/java/com/foundationbank/project/Controller/UserController.java
@@ -1,8 +1,67 @@
 package com.foundationbank.project.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+import com.foundationbank.project.Repositories.UserRepository;
+import com.foundationbank.project.Services.UserService;
+import com.foundationbank.project.payload.DTO.UserDTO;
+import com.foundationbank.project.payload.Response.UserResponse;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController()
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO){
+        UserDTO savedUserDTO = userService.createUser(userDTO);
+        return new ResponseEntity<>(savedUserDTO, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/find")
+    public ResponseEntity<List<UserDTO>> listAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("update/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO) {
+
+        UserDTO updatedUser = userService.updateUser(userDTO, userId);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+
+        
+    }
+
+    @DeleteMapping("/remove/{userId}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable Long userId){
+        UserDTO deletedUser = userService.deleteUser(userId);
+        return new ResponseEntity<>(deletedUser, HttpStatus.OK);
+    }
+    
+
+    
     
 }
